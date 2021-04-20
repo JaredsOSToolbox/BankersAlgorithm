857abf18f1c5576e8cfdb7239e3f7853edc83ec4
JaredDyreson <jared.dyreson@gmail.com>
2021-04-20 15:10:42 -0700

    removed unecessary components

--- includes/args_t.hpp
+++
@@ -1,9 +0,0 @@
-#include "../includes/banker_t.hpp"
-#include "../includes/customer_t.hpp"
-
-struct args_t {
-  customer_t* customer;
-  banker_t* banker;
-  int value;
-} args_t;
-
--- tests/safety.cpp
+++
@@ -1,7 +0,0 @@
-//Safety algorithm
-//
-
-
-bool safety(customer_t* customer) {
-  EVec::extended_vector_t<int> available_resources = this->available_resources;
-}
